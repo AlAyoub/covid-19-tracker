7c53cf40f137e58fe33b910a0ae99911d92dcbd4
Alan Ayoub <alanayoub@me.com>
2020-10-01 06:38:02 -0700

    homepage framing almost done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { MenuItem, FormControl, Select } from '@material-ui/core';
+import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core';
 import InfoBox from './InfoBox';
+import Map from './Map';
+import Header from './Header'
 
 function App() {
   const [countries, setCountires] = useState([]);
   const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
     const getCountiresData = async () => {
@@ -30,48 +33,69 @@ function App() {
     console.log('Country Code showing is >>>>>> ', countryCode)
 
     setCountry(countryCode);
-  }
 
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
+    await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      })
+  };
+
+  console.log('Country Info >>>>>>', countryInfo)
   return (
-    <div className="app">
-      <div className="app_header">
+    <div>
+      <Header />
+      <div className="app">
 
-        <h1>COVID-19 TRACKER</h1>
+        <div className="app_left">
+          <div className="app_header">
 
-        <FormControl className="app_dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {
-              countries.map(country =>
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              )
-            }
+            {/* Header */}
+            <h1>COVID-19 TRACKER</h1>
 
-          </Select>
-        </FormControl>
+            {/* Title + Select input dropdown field */}
+            <FormControl className="app_dropdown">
+              <Select variant="outlined" onChange={onCountryChange} value={country}>
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {countries.map(country => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          {/* InfoBoxes */}
+          {/* InfoBoxes */}
+          {/* InfoBoxes */}
+          <div className="app_stats">
+            <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={2000} />
+            <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={2000} />
+            <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={2000} />
+          </div>
+
+          {/* Map */}
+          <Map />
+
+        </div>
+        <Card className="app_right">
+          <CardContent>
+            <h3>Live Cases by Country</h3>
+            {/* Table */}
+            <h3>Worldwide new cases</h3>
+            {/* Graph */}
+          </CardContent>
+
+
+        </Card>
+
 
       </div>
-
-      <div className="app_stats">
-        <InfoBox title="Coronavirus Cases" cases={123} total={2000} />
-        <InfoBox title="Recovered" cases={1234} total={2000} />
-        <InfoBox title="Deaths" cases={12345} total={2000} />
-      </div>
-
-
-      {/* Header */}
-      {/* Title + Select input dropdown field */}
-
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
-      {/* InfoBoxes */}
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
-
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <img
+                className="header_logo"
+                src="https://healthy.kaiserpermanente.org/etc.clientlibs/settings/wcm/designs/kporg/kp-foundation/clientlib-modules/styleguide/resources/assets/images/logo.svg"
+                alt="Kaiser Permanente Logo"
+            />
+        </header>
+    )
+}
+
+export default Header
