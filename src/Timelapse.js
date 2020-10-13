0cfd2accb0a9935ca0840479dccee1d18de0535b
Alan Ayoub <alanayoub@me.com>
2020-10-13 06:40:23 -0700

    add files

---
+++ src/Timelapse.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Timelapse() {
+    return (
+        <div>
+            <iframe src="https://ourworldindata.org/grapher/total-cases-covid-19?tab=map&region=NorthAmerica" width="100%" height="600px"></iframe>
+        </div>
+    )
+}
+
+export default Timelapse
